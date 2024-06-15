ad8f410beb0fb4a73d8d9d70c022df1c238ae045
Maksimilian8 <162820834+Maksimilian8@users.noreply.github.com>
2024-06-15 18:40:59 +0300

    Add files via upload

---
+++ diary-login/database.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    with app.app_context():
+        db = get_db_connection()
+        with app.open_resource('schema.sql') as f:
+            db.executescript(f.read().decode('utf8'))
+        db.close()
